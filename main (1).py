0b1e708856a72f03f28045ecc97fbbc0594a5aeb
asanm10810822214002 <146019438+asanm10810822214002@users.noreply.github.com>
2023-09-26 19:33:18 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,23 +1,39 @@
-#Define the base class player
-class player:
-  def play(self):
-    print("the player is playing circket.")
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
 
-#Define the derived class Batsman
-class Batsman(player):
-  def play(self):
-    print("the batsman is batting.")
+class Student:
 
-#Define thw derived class Bowler
-class Bowler:
-  def play(self):
-    print("the bowler is Bowling.")
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
 
-#create object of Batsman and Bowler classes
-batsman=Batsman()
-bowler=Bowler()
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
 
-#call the play() method for each object
-batsman.play()
-bowler.play()
+
+# Example usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-class bank_account:
-  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
-    self.__account_number=account_number
-    self.__account_holder_name=account_holder_name
-    self.__account_balance=initial_balance
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-  def deposit(self,amount):
-    if amount>0:
-      self.__account_balance+=amount
-      #self.__account _balance=self.__account_balance+amount
-      print("Deposit ₹{}.New balance: ₹{}".format(amount,self.__account_balance))
-    else:
-       print("invalid deposit amount:")
 
-  def withdraw (self, amount):
-    if amount>0 and amount<= self.__account_balance:
-     self.__account_balance-=amount
-     print("Withdraw ₹{}.New balance:₹{}".format(amount, self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance." )
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-  def display_balance(self):
-     print("Account balance for {}(Account #{}):₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-#create an instance of the bank_account class 
-account=bank_account(account_number=123456789,account_holder_name="atm",initial_balance=5000.0)
+  return indices
 
-#test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
